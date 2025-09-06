05b1b27760070f0bf2714d5949f0c9ef87e69cc7
MarkCh20 <markiyanch20@gmail.com>
2025-09-06 20:02:08 +0300

    Added Security Config and JWT-based authentication and authorization

---
+++ src/main/java/org/decepticons/linkshortener/api/security/config/WebSecurityConfig.java
@@ -0,0 +1,15 @@
+package org.decepticons.linkshortener.api.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}

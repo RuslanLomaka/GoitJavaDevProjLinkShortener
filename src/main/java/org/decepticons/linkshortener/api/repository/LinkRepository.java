edd3dc31496cf30d695ac3cfa21cfe2278363e72
unknown <dima.itsko.ua@gmail.com>
2025-09-06 12:29:24 +0300

    fix: added imports :D

--- src/main/java/org/decepticons/linkshortener/api/repository/LinkRepository.java
+++ src/main/java/org/decepticons/linkshortener/api/repository/LinkRepository.java
@@ -1,8 +1,12 @@
 package org.decepticons.linkshortener.api.repository;
 
 import org.decepticons.linkshortener.api.model.Link;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.awt.print.Pageable;
+import java.util.Optional;
 
 @Repository
 public interface LinkRepository extends JpaRepository<Link, Long> {

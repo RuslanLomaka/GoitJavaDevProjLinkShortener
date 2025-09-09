045373183d1d2ba8025b3a8860a35c1a53b30e10
ruslan <wfptny@gmail.com>
2025-09-09 17:28:58 +0200

    linlshortenerApplication now makes checkstyle happy

--- src/main/java/org/decepticons/linkshortener/LinkShortenerApplication.java
+++ src/main/java/org/decepticons/linkshortener/LinkShortenerApplication.java
@@ -3,11 +3,21 @@ package org.decepticons.linkshortener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the Link Shortener application.
+ * This class bootstraps the Spring Boot application by calling
+ * {@link SpringApplication#run(Class, String...)}.
+ * </p>
+ */
 @SpringBootApplication
 public class LinkShortenerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LinkShortenerApplication.class, args);
-	}
-
+  /**
+   * Main method that starts the Link Shortener application.
+   *
+   * @param args command-line arguments passed during application startup
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(LinkShortenerApplication.class, args);
+  }
 }

92670cbb1f8fba058b5fc239c4e58dcfa1f61890
wfptn <wfptny@gmail.com>
2025-09-14 00:28:27 +0200

    fixed minor Swagger issues

--- src/main/java/org/decepticons/linkshortener/config/OpenApiConfig.java
+++ src/main/java/org/decepticons/linkshortener/config/OpenApiConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class OpenApiConfig {
+
   /**
    * Defines a GroupedOpenApi bean for version 1 of the API.
    * Includes only versioned paths ("/api/v1/**").
@@ -17,7 +18,8 @@ public class OpenApiConfig {
   @Bean
   public GroupedOpenApi apiV1Group() {
     return GroupedOpenApi.builder()
-        .group("v1")
+        .group("api-v1")
+        .packagesToScan("org.decepticons.linkshortener.api.v1.controller")
         .pathsToMatch("/api/v1/**")
         .build();
   }
@@ -29,8 +31,8 @@ public class OpenApiConfig {
   @Bean
   public GroupedOpenApi publicGroup() {
     return GroupedOpenApi.builder()
-        .group("public")
-        .pathsToMatch("/health", "/{code}")
+        .group("public")                 // label in Swagger dropdown
+        .pathsToMatch("/api/links/**", "/health")
         .build();
   }
 }
